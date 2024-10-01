2f5cae9e5950cd9fad7fd6ac94379b92122c93fe
aidarhaziahmetoff <aidarhaziahmetoff@yandex.ru>
2024-10-01 10:25:57 +0500

    01.10.2024

--- app/routes.py
+++ app/routes.py
@@ -7,43 +7,52 @@ from time import sleep
 import sqlalchemy as sa
 
 
-@app.route("/")
-@app.route("/index")
-def index():
-    user = {"username": "Miguel"}
-    posts = [
-        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
-        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
-    ]
-    return render_template("index.html", title="Home", user=user, posts=posts)
+
+# @app.route("/index")
+# def index():
+#     user = {"username": "Miguel"}
+#     posts = [
+#         {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+#         {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+#     ]
+#     return render_template("index.html", title="Home", user=user, posts=posts)
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    if request.method == "POST":
-        if form.validate_on_submit():
-            flash(
-                "Login requested for user {}, remember_me={}".format(
-                    form.username.data, form.remember_me.data
-                )
-            )
-            return redirect(url_for("index"))
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#     form = LoginForm()
+#     if request.method == "POST":
+#         if form.validate_on_submit():
+#             flash(
+#                 "Login requested for user {}, remember_me={}".format(
+#                     form.username.data, form.remember_me.data
+#                 )
+#             )
+#             return redirect(url_for("index"))
+#     if request.method == "GET":
+#         if (
+#             request.args.get("username") is not None
+#             and request.args.get("password") is not None
+#         ):
+#             flash(
+#                 "Login requested for user {}, remember_me={}".format(
+#                     form.data.get("username"), form.data.get("remember_me")
+#                 )
+#             )
+#             sleep(10)
+#             return redirect(url_for("index"))
+#         return render_template("login.html", title="Вход", form=form)
+
+# @app.route("/reg",)
+@app.route("/", methods=["GET", "POST"])
+def registration():
     if request.method == "GET":
         if (
-            request.args.get("username") is not None
-            and request.args.get("password") is not None
+            request.args.get("registration_user_password") is not None
+            and request.args.get("confirm_registration_user_password") is not None
         ):
-            flash(
-                "Login requested for user {}, remember_me={}".format(
-                    form.data.get("username"), form.data.get("remember_me")
-                )
-            )
-            sleep(10)
-            return redirect(url_for("index"))
-        return render_template("login.html", title="Вход", form=form)
-
-
-@app.route("/reg", methods=["GET", "POST"])
-def registration():
+            pass
+        elif request.args.get("registration_user_password") != request.args.get("confirm_registration_user_password"):
+            pass
     return render_template("loginv1.html")
+
